005c12518f28e9f2b0472b0947560f2d65181c1d
Deb56xy <debajyxy@gmail.com>
2026-03-14 14:22:07 +0530

    Update authority-dashboard.js

--- frontend/js/authority-dashboard.js
+++ frontend/js/authority-dashboard.js
@@ -64,7 +64,7 @@ function getInitials(name) {
 
 function logout() {
     localStorage.clear();
-    window.location.href = "login.html";
+    window.location.href = "index.html";
 }
 
 /***********************
